4a81ae577df765db75317d5c6bd8ae3bfd949cc4
Tony.li <tony.li@gaiaworks.cn>
2019-11-08 17:52:40 +0800

    其他页面

---
+++ js/page/MyPage.js
@@ -0,0 +1,41 @@
+
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import NavigationUtill from '../navigator/NavigationUtill'
+
+export default class MyPage extends Component {
+  componentDidMount(){
+    this.timer = setTimeout(() => {
+      NavigationUtill.resetToHomePage({
+        navigation: this.props.navigation
+      })
+    },2000)
+  }
+
+  componentWillUnMount(){
+    this.timer && clearTimeout(this.timer); 
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>MyPage</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
