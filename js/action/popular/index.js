093945786996c168d0639745b5d11e2571815c60
Tony.li <tony.li@gaiaworks.cn>
2019-11-22 17:49:21 +0800

    添加redux

---
+++ js/action/popular/index.js
@@ -0,0 +1,78 @@
+import Types from '../types';
+import DataStore from '../../expand/dao/DataStore';
+
+export function onRefreshPopular(storeName,url,pageSize){
+    return dispatch =>{
+        dispatch({type:Types.POPULAR_REFRESH, storeName: storeName});
+        console.log('debug-->','刷新');
+        let dataStore = new DataStore();
+        dataStore.fetchData(url)
+            .then(data=>{
+                handleData(dispatch,storeName,data,pageSize);
+            })
+            .catch(error =>{
+                console.log('debug-->','失败');
+                console.log('类型--》',error);
+                dispatch({
+                    type:Types.LOAD_POPULAR_FAIL,
+                    storeName:storeName,
+                    error
+                })
+            })
+    }
+}
+
+/**
+ * 加载更多
+ * @param storeName
+ * @param pageIndex 第几页
+ * @param pageSize 每页展示条数
+ * @param dataArray 原始数据
+ * @param callBack 回调函数，可以通过回调函数来向调用页面通信：比如异常信息的展示，没有更多等待
+ * @param favoriteDao
+ * @returns {function(*)}
+ */
+export function onLoadMorePopular(storeName, pageIndex, pageSize, dataArray = [], callBack) {
+    return dispatch => {
+        setTimeout(() => {//模拟网络请求
+            if ((pageIndex - 1) * pageSize >= dataArray.length) {//已加载完全部数据
+                if (typeof callBack === 'function') {
+                    callBack('no more')
+                }
+                dispatch({
+                    type: Types.POPULAR_LOAD_MORE_FAIL,
+                    error: 'no more',
+                    storeName: storeName,
+                    pageIndex: --pageIndex,
+                })
+            } else {
+                //本次和载入的最大数量
+                let max = pageSize * pageIndex > dataArray.length ? dataArray.length : pageSize * pageIndex;
+                dispatch({
+                    type: Types.POPULAR_LOAD_MORE_SUCCESS,
+                    storeName,
+                    pageIndex,
+                    projectModels: dataArray.slice(0, max),
+                })
+               
+            }
+        }, 500);
+    }
+}
+
+function handleData(dispatch,storeName,data,pageSize) {
+    console.log('类型--》',storeName);
+    console.log('类型--》',data.data.items.length);
+    console.log('debug-->','成功');
+    let fixItems =[];
+    if(data && data.data && data.data.items){
+        fixItems = data.data.items
+    }
+    dispatch({
+        type:Types.LOAD_POPULAR_SUCCESS,
+        items:fixItems,
+        storeName:storeName,
+        projectModels:pageSize > fixItems.length ? fixItems : fixItems.slice(0, pageSize),//第一次要加载的数据
+        pageIndex:1
+    })
+}
---
+++ js/reducer/popular/index.js
@@ -0,0 +1,61 @@
+import Types from '../../action/types'
+
+const initState = {
+   
+}
+
+export default function onAction (state = initState, action ){
+    switch(action.type){
+        case Types.LOAD_POPULAR_SUCCESS://下拉刷新成功
+            return {
+                ...state,
+                [action.storeName]: {
+                    ...state[action.storeName],
+                    items: action.items,//原始数据
+                    projectModels: action.projectModels,//此次要展示的数据
+                    isLoading: false,
+                    hideLoadingMore: false,
+                    pageIndex: action.pageIndex
+                }
+            };
+        case Types.POPULAR_REFRESH://下拉刷新
+            return {
+                ...state,
+                [action.storeName]: {
+                    ...state[action.storeName],
+                    isLoading: true,
+                    hideLoadingMore: true,
+                }
+            }
+        case Types.LOAD_POPULAR_FAIL:
+                return {
+                    ...state,
+                    [action.storeName]: {
+                        ...state[action.storeName],
+                        isLoading: true,
+                    }
+                }
+        case Types.POPULAR_LOAD_MORE_SUCCESS:
+            return {
+                ...state,
+                [action.storeName]: {
+                    ...state[action.storeName],
+                    projectModels: action.projectModels,
+                    hideLoadingMore: false,
+                    pageIndex: action.pageIndex,
+                }
+            }
+        case Types.POPULAR_LOAD_MORE_FAIL://上拉加载更多失败
+        return {
+            ...state,//Object.assign @http://www.devio.org/2018/09/09/ES6-ES7-ES8-Feature/
+            [action.storeName]: {
+                ...state[action.storeName],
+                hideLoadingMore: true,
+                pageIndex: action.pageIndex,
+            }
+        };
+        default:
+            return state;
+        
+    }
+}
