562686773d44740153d36621fed10b8e3a2f521a
Tony.li <tony.li@gaiaworks.cn>
2019-11-22 17:52:06 +0800

    添加最热页面逻辑

---
+++ js/common/PopularItem.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View,} from 'react-native'
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+export default class PopularItem extends Component {
+
+    render() {
+        const {item,onSelect} = this.props;
+        if(!item||!item.owner) return null;
+        return(
+            <TouchableOpacity
+              onPress={()=>{onSelect && onSelect()}}
+            >
+               <View style={styles.cell_container}>
+                   <Text style={styles.title}> {item.full_name}</Text>
+                   <Text style={styles.description}> {item.description}</Text>
+                   <View style={styles.row}>
+                        <View style={styles.row}>
+                            <Text>Author:</Text>
+                            <Image style={{height: 22, width: 22}}
+                                   source={{uri: item.owner.avatar_url}}
+                            />
+                        </View>
+                   </View>
+               </View>
+            </TouchableOpacity>
+        );
+        
+    }
+}
+
+const styles = StyleSheet.create({
+    cell_container: {
+        backgroundColor: 'white',
+        padding: 10,
+        marginLeft: 5,
+        marginRight: 5,
+        marginVertical: 3,
+        borderColor: '#dddddd',
+        borderWidth: 0.5,
+        borderRadius: 2,
+        elevation: 2
+    },
+    title: {
+        fontSize: 16,
+        marginBottom: 5,
+        color: '#212121',
+    },
+    description: {
+        fontSize: 14,
+        color: '#757575',
+        lineHeight:22,
+    },
+    row: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },  
+}
+);
